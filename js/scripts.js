aa692b80c4efeda6dfb4fc1e70c9ac32e6cf4729
Debbie Michel and Linda Luu <debbie@>
2017-08-09 10:21:00 -0700

    add debugging tools

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,15 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+  $("#blanks form").submit(function(event) {
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      console.log(userInput);
+      $("." + blank).text(userInput).val();
+    });
 
     $("#story").show();
+
     event.preventDefault();
   });
 });
